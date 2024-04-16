b7cda6dc085c117b61cfe168eeeb5c20b6858916
Joa0DeL1ma <161715327+Joa0DeL1ma@users.noreply.github.com>
2024-04-16 10:03:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+    message: "Digite o URL: ",
+    name: "URL"
+    }
+
+  ])
+  .then((answers) => {
+    let url = answers.URL
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('url.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
